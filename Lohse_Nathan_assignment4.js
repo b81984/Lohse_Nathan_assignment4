971bc986d22a04c9d2863a473d1a005acab6eafa
b81984 <lordxchris@gmail.com>
2013-09-26 10:09:04 -0500

    5 functions working

--- Lohse_Nathan_assignment4.js
+++ Lohse_Nathan_assignment4.js
@@ -32,6 +32,59 @@ var fuzzyMatch = function() //Is the Number above or below a number within a cer
 
 var timeDifference = function()
 {
+	var date1, date2, month1, month2, day1, day2, year1, year2, gap, monthDiff, dayDiff, yearDiff,monthTrack;
+	gap = 0;
+	date1 = prompt("Enter the first date in MM/DD/YYYY format.");
+	date2 = prompt("Enter the second date in MM/DD/YYYY format.");
+	month1 = parseInt(date1.substring(0,2));
+	month2 = parseInt(date2.substring(0,2));
+	day1 = parseInt(date1.substring(3,5));
+	day2 = parseInt(date2.substring(3,5));
+	year1 = parseInt(date1.substring(6,10));
+	year2 = parseInt(date2.substring(6,10));
+	monthDiff = month2 - month1;
+	dayDiff = day2 - day1;
+	yearDiff = year2 - year1;
+	monthTrack = month1;
+	//console.log(date1 + " " + month1 + " " + day1 + " " + year1);
+	//console.log(date2 + " " + month2 + " " + day2 + " " + year2);
+
+	if (yearDiff < 0)
+	{
+		for(var i = 0; i > yearDiff; i--)
+		{
+			gap = gap + 365;
+		}
+	}
+	else
+	{
+		for( var i = 0; i < yearDiff; i++)
+		{
+			gap = gap + 365;
+		}
+	}
+
+
+	if (monthDiff < 0)
+	{
+		monthDiff = 12 + monthDiff;
+		for(var i = 0; i > monthDiff; i--)
+		{
+
+
+		}
+
+	}
+	else
+	{
+		for( var i = 0; i < monthDiff; i++)
+		{
+
+		}
+
+	}
+
+
 	
 }
 
@@ -52,11 +105,44 @@ var isPhoneNumber = function()
 
 var isURL = function()
 {
-	
+	var testString;
+	testString = prompt("Enter a string to test if it follows a URL format (Begins with http: or https:");
+	if(testString.substring(0,5) === "http:" || testString.substring(0,6) === "https:")
+	{
+		console.log("This is a URL!");
+	}
+	else
+	{
+		console.log("This is NOT a URL!");
+	}
 }
 
 var smallestValue = function()
 {
+	var check;
+	var set = [1, 74, 5, 16, 9, 13, 17, 27, 43, 46, 58, 2, 12, 7];
+	var toReturn;
+	var foundAny = false;
+	check = parseInt(prompt("Enter a number.  The function will find the smallest value in a preset array that is larger than your number."));
+	console.log("The preset array is " +set);
+	for(var i = 0; i < set.length; i++)
+	{
+		if(foundAny === false)
+		{
+			if(check < set[i])
+			{
+				toReturn = set[i];
+				foundAny = true;
+			}
+
+		}
+		else if(set[i] < toReturn && set[i] > check)
+		{
+			toReturn = set[i];
+		}
+
+	}
+	console.log("The number that best fits the conditions is " + toReturn);
 	
 }
 
