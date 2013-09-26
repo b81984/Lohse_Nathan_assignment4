7b0ea0d785b0c79939d60a4771038281ac3c06e3
b81984 <lordxchris@gmail.com>
2013-09-26 06:56:54 -0500

    Setup function outline

---
+++ Lohse_Nathan_assignment4.js
@@ -0,0 +1,31 @@
+alert("JavaScript works!");
+
+var fuzzyMatch = function()
+{
+
+}
+
+var timeDifference = function()
+{
+	
+}
+
+var isPhoneNumber = function()
+{
+	
+}
+
+var isURL = function()
+{
+	
+}
+
+var smallestValue = function()
+{
+	
+}
+
+var stringToNumber = function()
+{
+	
+}
