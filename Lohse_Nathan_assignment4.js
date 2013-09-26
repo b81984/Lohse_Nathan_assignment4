977fd7136b29decb8c00f7c381010f5354ab03d2
b81984 <lordxchris@gmail.com>
2013-09-26 08:15:12 -0500

    Tested all written code, working as expected

--- Lohse_Nathan_assignment4.js
+++ Lohse_Nathan_assignment4.js
@@ -2,6 +2,7 @@ alert("JavaScript works!");
 
 var goOn = true;
 var select = 0;
+var selectNum = 0;
 
 var fuzzyMatch = function() //Is the Number above or below a number within a certain percent?
 {
@@ -15,6 +16,7 @@ var timeDifference = function()
 
 var isPhoneNumber = function()
 {
+	console.log("Check 1");
 	var testNumber = "";
 	testNumber = prompt("Enter a string to test for phone number format (must include XXX-XXX-XXXX format to be considered a phone number)");
 	if(testNumber.indexOf("-") === 3 && testNumber.lastIndexOf("-") === 7)
@@ -46,52 +48,54 @@ var stringToNumber = function()
 	if(toChange.indexOf(".") === -1)
 	{
 		changed = parseInt(toChange);
+		console.log(changed + " has been converted to an Int data item!");
 	}
 
 	else
 	{
 		changed = parseFloat(toChange);
+		console.log(changed + " has been converted to a Float data item!");
 	}
-	console.log(changed + "has been converted to a number data item!");
 }
 
 while(goOn === true)
 {
-	console.log("List of functions available:\n1. fuzzyMatch - Fuzzy-match a number: is the number above or below a number within a certain percent?\n2. timeDifference - Find the number of hours or days difference between two dates.\n3. isPhoneNumber - Does a string follow a 123-456-7890 pattern like a phone number?\n4. isURL - Is the string a URL? (Does it start with http: or https:?)\n5. smallestValue - Find the smallest value in an array that is greater than a given number.\n6. stringToNumber - Given a string version of a number such as "42", return the value as an actual Number, such as 42");
+	console.log("List of functions available:\n1. fuzzyMatch - Fuzzy-match a number: is the number above or below a number within a certain percent?\n2. timeDifference - Find the number of hours or days difference between two dates.\n3. isPhoneNumber - Does a string follow a 123-456-7890 pattern like a phone number?\n4. isURL - Is the string a URL? (Does it start with http: or https:?)\n5. smallestValue - Find the smallest value in an array that is greater than a given number.\n6. stringToNumber - Given a string version of a number such as \"42\", return the value as an actual Number, such as 42");
 	select = prompt("Select a function from the list, using the function's designated number");
-	if(select === 1)
+	selectNum = parseInt(select);
+	if(selectNum === 1)
 	{
 		fuzzyMatch();
 	}
 
-	else if(select === 2)
+	else if(selectNum === 2)
 	{
-
+		timeDifference();
 	}
 
-	else if(select === 3)
-	{
-		
-	}
-
-	else if(select === 4)
+	else if(selectNum === 3)
 	{
 		
+		isPhoneNumber();		
 	}
 
-	else if(select === 5)
+	else if(selectNum === 4)
 	{
-		
+		isURL();
 	}
 
-	else if(select === 6)
+	else if(selectNum === 5)
 	{
-		
+		smallestValue();
 	}
 
+	else if(selectNum === 6)
+	{
+		stringToNumber();		
+	}
 	else
 	{
-
+		console.log("You've selected an invalid choice.  Please try again, if you'd still like to run a function");
 	}
 
 	goOn = confirm("Perform another function?");
